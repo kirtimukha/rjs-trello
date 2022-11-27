c03e6f6abed20895757ac4300a49b70a533f59e4
kirti <kirtimukha@nate.com>
2022-11-27 19:13:33 +0900

    [U] react hook form

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-
 import App from './App';
-
 import {QueryClient, QueryClientProvider} from "react-query";
 import {RecoilRoot} from "recoil";
 import {ThemeProvider} from "styled-components";
