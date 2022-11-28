cb6929226fd7b77d64b2a074426d9b958af762b5
kirti <kirtimukha@nate.com>
2022-11-28 22:29:14 +0900

    [U] selector 카테고리별로 리턴하기

--- src/atom/atom.tsx
+++ src/atom/atom.tsx
@@ -9,21 +9,21 @@ export interface IToDo {
   id: number;
   category: "TO_DO" | "DOING" | "DONE";
 }
-export const toDoStateAtom = atom<IToDo[]>({
+export const categoryState = atom({
+key: "category",
+  default: "TO_DO"
+})
+
+export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default:[],
 })
 
 export const toDoSelector = selector({
-  //atom 스테이트를 변환시키는 용도로 셀렉터를 사용한다.
   key:"toDoSelector",
   get: ({ get }) => {
-   // const toDos = get(toDoStateAtom);
-    const toDos = get(toDoStateAtom)
-    return [
-      toDos.filter((toDo) => toDo.category === "TO_DO"),
-      toDos.filter((toDo) => toDo.category === "DOING"),
-      toDos.filter((toDo) => toDo.category === "DONE"),
-    ];
+    const toDos = get(toDoState)
+    const category = get(categoryState)
+    return toDos.filter((toDo) => toDo.category === category)
   }
 })
