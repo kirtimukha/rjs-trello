7a5ba0edbce31c53c421c51af057ff7f57d9beb9
stella <kirtimukha@nate.com>
2023-01-05 18:59:32 +0900

    [U] Time update - selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import styled, {createGlobalStyle} from "styled-components";
-const GlobalStyle=createGlobalStyle`
-    *{box-sizing:border-box; }
-    @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
+import {useRecoilState, useRecoilValue} from "recoil";
+import {hourSelector, minuteState} from './atom/atom';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
 `
 
-const App = () => {
-return (
+function App() {
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value)
+  }
+  const hours = useRecoilValue(hourSelector);
+
+  return (
     <>
-        <GlobalStyle />
+      <GlobalStyle/>
+      <div>
+        <input type="number" placeholder="Minutes" defaultValue={minutes} onChange={onMinutesChange}/>
+        <input type="number" placeholder="Hours" value={hours} readOnly/>
+      </div>
     </>
-)
+  )
 }
 
 export default App;
--- src/atom/atom.tsx
+++ src/atom/atom.tsx
@@ -1,2 +1,15 @@
 import {atom, selector} from "recoil";
 //selector : state 를 입력 받는 함수를 거쳐서 반환된 값을 말함
+
+export const minuteState = atom({
+    key: "minutes",
+    default: 0,
+})
+
+export const hourSelector = selector({
+    key: "hours",
+    get: ({ get }) => {
+        const minutes = get(minuteState);
+        return minutes/60 ;
+    }
+})
