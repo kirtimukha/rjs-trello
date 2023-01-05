9d2d229f99267827ad0552ca64dc559716b3ae99
kirti <kirtimukha@nate.com>
2023-01-05 16:44:36 +0900

    [C] first Create

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled, {createGlobalStyle} from "styled-components";
-import TodoList from "./TodoList";
 const GlobalStyle=createGlobalStyle`
     *{box-sizing:border-box; }
     @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
