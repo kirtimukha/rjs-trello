d7615d08816abe3e3bc74b32d74c866b5ac44d16
stella <kirtimukha@nate.com>
2023-01-06 15:12:24 +0900

    [U] selector set 속성 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import styled, {createGlobalStyle} from "styled-components";
-import {useRecoilState, useRecoilValue} from "recoil";
+import {createGlobalStyle} from "styled-components";
+import {useRecoilState} from "recoil";
 import {hourSelector, minuteState} from './atom/atom';
 
 const GlobalStyle = createGlobalStyle`
@@ -13,17 +13,26 @@ const GlobalStyle = createGlobalStyle`
 
 function App() {
   const [minutes, setMinutes] = useRecoilState(minuteState);
+  const [hours, setHours] = useRecoilState(hourSelector);
+  //useRecoilState( A ) : A에는 아톰이름 혹은 셀렉터 이름을 쓸 수 있다.
+  //useRecoilState 를 쓸 때, 결과 []의 첫번째 item  은  atom 값이거나, selector의 get 함수의 값이다.
+  //useRecoilState의 두번째 요소는 atom을 수정하거나 selector의  set property를 실행시키는 함수이다.
   const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
     setMinutes(+event.currentTarget.value)
   }
-  const hours = useRecoilValue(hourSelector);
+
+  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value)
+  }
+
+ // const hours = useRecoilValue(hourSelector);
 
   return (
     <>
       <GlobalStyle/>
       <div>
-        <input type="number" placeholder="Minutes" defaultValue={minutes} onChange={onMinutesChange}/>
-        <input type="number" placeholder="Hours" value={hours} readOnly/>
+        <input type="number" placeholder="Minutes" value={minutes} onChange={onMinutesChange}/>
+        <input type="number" placeholder="Hours" value={hours} onChange={onHoursChange}/>
       </div>
     </>
   )
