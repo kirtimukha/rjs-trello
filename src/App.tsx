7d2f640d07a27c0ee1b8795c78f2b19575c02762
stella <stella.park.korea@gmail.com>
2023-01-08 20:57:57 +0900

    [D] DraggableCard로 컴포넌트 리팩토링 설명 주석 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -140,20 +140,6 @@ function App() {
                 <Board ref={magic.innerRef} {...magic.droppableProps}>
                   {toDos.map((toDo, index) => (
                     <DragabbleCard key={toDo} toDo={toDo} index={index} />
-                    // <Draggable draggableId={toDo} key={toDo} index={index}>
-                    //   {/*//드래거블 아이디에 ""(큰따옴표) 로 이름을 주면 Card 드래그 드랍시 1개만 보이므로 {}(대괄호)사용.*/}
-                    //   {/*//드래거블 아이디와 key 네임이 같아야 한다 */}
-                    //   {(magic) => (
-                    //     <Card
-                    //       ref={magic.innerRef}
-                    //       {...magic.draggableProps}
-                    //       {...magic.dragHandleProps}
-                    //       onChange={changeToDo}
-                    //     >
-                    //       {toDo}
-                    //     </Card>
-                    //   )}
-                    // </Draggable>
                   ))}
                   {magic.placeholder}
                 </Board>
--- src/Components/DragabbleCard.tsx
+++ src/Components/DragabbleCard.tsx
@@ -24,8 +24,6 @@ function DragabbleCard({toDo, index }: IDraggableCardProps)  {
 return (
   <>
     <Draggable  key={toDo} draggableId={toDo} index={index}>
-      {/*//드래거블 아이디에 ""(큰따옴표) 로 이름을 주면 Card 드래그 드랍시 1개만 보이므로 {}(대괄호)사용.*/}
-      {/*//드래거블 아이디와 key 네임이 같아야 한다 */}
       {(magic) => (
         <Card
           ref={magic.innerRef}
